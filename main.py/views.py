28589628d69a088fad0be030ccad7eb211f6f67d
Amit Joe <joe.amit@yahoo.com>
2023-04-23 21:48:03 +0530

    Create views.py
    This is the function based view in which all the business logic is created.

---
+++ main.py/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from mainapp.forms import *
+
+def index_view(request):
+    return render(request,'index.html')
+
+
+def bf_view(request):
+    form=bf_form()
+    return render(request,"bf.html",{'form':form,})
+
+def gf_view(request):
+    bf_name=request.GET['bname']
+    form=gf_form()
+    request.session['bname']=bf_name
+    return render(request,"gf.html",{'form':form,'bf_name':bf_name})
+
+
+def age_view(request):
+    bf_name=request.session.get('bname')
+    gf_name=request.GET['gname']
+    form=age_form()
+    request.session['gname']=gf_name
+    return render(request,"age.html",{'form':form,'bf_name':bf_name})
+
+
+def result_view(request):
+    bf_name=request.session.get('bname')
+    gf_name=request.session.get('gname')
+    bage=request.GET['age']
+    request.session['age']=bage
+    return render(request,"result.html",{'bf_name':bf_name,'gf_name':gf_name,'bage':bage})
